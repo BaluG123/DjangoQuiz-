dacb0dc1f79cff95bcaa15c85a9dfab89ccfc367
balu <balu1072000@gmail.com>
2022-08-15 14:27:30 +0530

    made some changes

--- DjangoQuiz1/settings.py
+++ DjangoQuiz1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p#$mb!@sj0(a7vf53cm+87h008(gl8zmaac)0*fyv+1x&sq-91'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
